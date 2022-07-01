da558109cd5d8c03ecea1df7aef1ab6b79326350
jeffsummers <jeff.summers@patientzero.com.au>
2022-07-01 13:21:14 +1000

    Add swagger config

---
+++ api/src/main/java/com/tracker/api/config/SwaggerConfiguration.java
@@ -0,0 +1,23 @@
+package com.tracker.api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfiguration {
+  @Bean
+  public OpenAPI customOpenAPI(@Value("${app.version}") String appVersion) {
+    OpenAPI openapi = new OpenAPI()
+        .components(new Components())
+        .info(new Info()
+            .title("Expense Tracker API")
+            .version(appVersion));
+
+    return openapi;
+  }
+}
